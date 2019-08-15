00a1a34a67f8893f1a80c518c9005ef12abe4923
Ubuntu <ubuntu@ip-172-31-28-48.ap-southeast-1.compute.internal>
2019-08-15 05:03:38 +0000

    Added upload for images

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField(blank=False)
     created_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to='images/', null=True)
     
     def __str__(self):
         return self.title
