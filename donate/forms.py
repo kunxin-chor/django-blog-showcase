9764a343afac4501b00a0abadd4e9b96ca26ab85
Ubuntu <ubuntu@ip-172-31-28-48.ap-southeast-1.compute.internal>
2019-08-16 01:49:52 +0000

    Able to make the form shows up

--- donate/forms.py
+++ donate/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Order
 
-class MakePaymentForm(forms.Form):
+class PaymentForm(forms.Form):
 
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
--- donate/views.py
+++ donate/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
+from .forms import *
 
 # Create your views here.
 def donate(request):
@@ -6,8 +8,14 @@ def donate(request):
     
 def charge(request):
     if request.method == 'GET':
-        pass
         #show form
+        return render(request, 'donate/charge.html', {
+            'publishable' : settings.STRIPE_PUBLISHABLE_KEY,
+            'payment_form' : PaymentForm(),
+            'order_form' : OrderForm()
+        })
+    
     else:
         pass
         #process form
+        
