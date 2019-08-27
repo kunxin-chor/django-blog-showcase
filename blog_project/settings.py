b82e2b33347e04ec8fa4391637654f0735756685
Ubuntu <ubuntu@ip-172-31-28-48.ap-southeast-1.compute.internal>
2019-08-27 09:41:35 +0000

    Using MySQL

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -77,14 +77,33 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+       'default': {
+        # MySQL database engine class.
+        'ENGINE': 'django.db.backends.mysql',
+        # MySQL database host ip.
+        'HOST': 'extrakun.mysql.pythonanywhere-services.com',
+        # port number.
+        'PORT': '3306',
+        # database name.
+        'NAME': 'dev2qa',
+        # user name.
+        'USER': 'extrakun',
+        # password
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        # connect options
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
